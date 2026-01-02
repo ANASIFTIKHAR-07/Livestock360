88d6885fd72051a7feaabcb2ec4b7638eb147e62
ANAS IFTIKHAR <131451392+ANASIFTIKHAR-07@users.noreply.github.com>
2026-01-02 18:58:14 +0500

    app.js + index.js

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -5,9 +5,16 @@ import cookieParser from "cookie-parser"
 
 const app = express()
 
+
+app.use(cors({
+    credentials: true,
+    origin: process.env.CORS_ORIGIN,
+}))
+
 app.use(cookieParser())
 app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({limit: "16kb", extended: true}))
+app.use(express.static("public"))
 
 
 
---
+++ Backend/src/index.js
@@ -0,0 +1,19 @@
+import connectDB from "./db/index.js";
+import {app} from "./app.js"
+import "dotenv/config"
+
+
+connectDB()
+.then(()=> {
+    app.on("error", (error)=> {
+        console.log("Server is not running at the PORT, Please check you PORT.", error);
+        throw error
+    })
+    // start server
+    app.listen(process.env.PORT || 4000, ()=> {
+        console.log(`🚀 Server is running at PORT : ${process.env.PORT}`);
+    })
+})
+.catch((error)=> {
+    console.log("❌ MongoDB connnetion Failed", error)
+})
