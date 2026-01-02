e40117b774519acc19f487b39277aca992b71321
ANAS IFTIKHAR <131451392+ANASIFTIKHAR-07@users.noreply.github.com>
2026-01-02 19:25:47 +0500

    accessrefreshtoken + getcurrentuser

--- Backend/src/controllers/user.controller.js
+++ Backend/src/controllers/user.controller.js
@@ -18,7 +18,7 @@ const addAccessAndRefreshToken = async (userId)=> {
     }
 }
 
-const login = asyncHandler(async()=> {
+const login = asyncHandler(async(req, res)=> {
     const {email, password} = req.body;
 
     if(!email || !password) {
@@ -64,7 +64,7 @@ const login = asyncHandler(async()=> {
 
 
 
-const logout = asyncHandler( async ()=> {
+const logout = asyncHandler( async (req, res)=> {
     await User.findByIdAndUpdate(
         req.user._id,
         {
@@ -95,8 +95,75 @@ const logout = asyncHandler( async ()=> {
     )
 })
 
+const accessRefreshToken = asyncHandler( async(req, res)=> {
+    const incommingRefreshToken = req.cookies.refreshToken || req.body.refreshToken
+
+    if(!incommingRefreshToken){
+        throw new ApiError(401, "Unauthorized Request !")
+    }
+    try {
+        const decodedToken = jwt.verify(
+            incommingRefreshToken,
+            process.env.REFRESH_TOKEN_SECRET,
+        )
+
+    const user = await User.findById(decodedToken._id);
+    if (!user) {
+        throw new ApiError(401, "Invalid Refresh Token!")
+    }
+
+    if (incomingRefreshToken !== user?.refreshToken) {
+        throw new ApiError(401, "Refresh Token is expired or used.")
+    }
+
+    const options = {
+        httpOnly: true,
+        secure: true,
+    }
+
+    
+    const {accessToken, refreshToken} = await addAccessAndRefreshToken(user._id)
+
+    return res
+    .status(200)
+    .cookie("accessToken", accessToken, options )
+    .cookie("refreshToken", refreshToken, options)
+    .json(
+        new ApiResponse(
+            200,
+            {
+                accessToken,
+                refreshToken: refreshToken,
+            },
+            "Access Token is refreshed successfully"
+        )
+    )
+    } catch (error) {
+        throw new ApiError(401, error?.message || "Invalide Refresh Token!")
+    }
+})
+
+
+const getCurrentUser = asyncHandler(async(req, res)=> {
+    if(!req.user){
+        throw new ApiError(404, "User not found!");
+    }
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(
+            200,
+            req.user,
+            "User Fetched Successfully."
+        )
+    )
+});
+
 
 export {
     login,
     logout,
+    accessRefreshToken,
+    getCurrentUser,
 }
