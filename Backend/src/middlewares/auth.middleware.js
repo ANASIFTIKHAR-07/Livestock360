a5a182befafde40ea8a6cc7ae7950295bb13350c
ANAS IFTIKHAR <131451392+ANASIFTIKHAR-07@users.noreply.github.com>
2026-01-05 17:04:20 +0500

    some more bug fixing

--- Backend/src/middlewares/auth.middleware.js
+++ Backend/src/middlewares/auth.middleware.js
@@ -24,6 +24,7 @@ export const verifyJWT = asyncHandler(async(req, res, next)=> {
             fullName: user.fullName,
             email: user.email,
         };
+        next()
     } catch (error) {
         throw new ApiError(401, error?.message || "Invalid Access Token!!")
     }
