861e43479d6ff9bf5b6e258cebce34d6cc1be107
ANAS IFTIKHAR <131451392+ANASIFTIKHAR-07@users.noreply.github.com>
2026-01-05 16:26:12 +0500

    animal controller bug fix

--- Backend/src/controllers/animal.controller.js
+++ Backend/src/controllers/animal.controller.js
@@ -1,7 +1,7 @@
-import asyncHandler from "../utils/asyncHandler.js"
-import ApiError from "../utils/ApiError.js"
-import ApiResponse from "../utils/ApiResponse.js"
-import Animal from "../models/animal.model.js"
+import {asyncHandler} from "../utils/asyncHandler.js"
+import {ApiError} from "../utils/ApiError.js"
+import {ApiResponse} from "../utils/ApiResponse.js"
+import { Animal } from "../models/animal.model.js"
 import { uploadOnCloudinary } from "../utils/cloudinary.js"
 
 
